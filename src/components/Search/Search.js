0bc774140c31204e4a097a9804ecd870d7bb52d9
Gabriel <gabriel_luchtenberg@edu.univali.br>
2018-11-04 18:25:34 -0200

    loader on search field

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import { cryptoApi } from '../../http';
+import { Loading } from '../';
 import './Search.css'
 class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            searchQuery: ''
+            searchQuery: '',
+            loading: false,
+            searchResults: []
         };
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange (event) {
         const searchQuery = event.target.value
-        this.setState({ searchQuery })
+        this.setState({ searchQuery, loading: true })
         if (!searchQuery) {
+            this.setState({ loading: false })
             return ''
         }
         cryptoApi.get(`autocomplete?searchQuery=${searchQuery}`)
-            .then(res => console.log(res))
+            .then(searchResults => this.setState({ searchResults, loading: false }))
     }
     render () {
+        const { loading } = this.state
         return (
             <div className="search-container">
                 <span className="search-icon" />
                 <input name="searchQuery" onChange={this.handleChange} placeholder="Search..." />
+                {
+                    loading && <div className="loading-search-container"><Loading height="12px" width="12px" /></div>
+                }
             </div>
         );
     }
